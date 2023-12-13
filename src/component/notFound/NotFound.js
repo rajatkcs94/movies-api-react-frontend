366be9566caa9e957e54631b14ae4eda08e0ce1e
rajatkcs94 <rajatk.cs94@gmail.com>
2023-12-13 17:36:15 +0530

    Initial commit

---
+++ src/component/notFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <h2 color="blue">This page could not be found!</h2>
+    </div>
+  )
+}
+
+export default NotFound
